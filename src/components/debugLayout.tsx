6489215894db4d5d619a5ec39d5c4c35ebae97cd
Xie Xiu Yue <xxy10062@gmail.com>
2019-04-18 23:30:58 +0800

    fix: Modify DEBUG Rendering Method

--- src/components/debugLayout.tsx
+++ src/components/debugLayout.tsx
@@ -1,20 +1,37 @@
 import * as React from 'react'
 import {connect} from 'react-redux'
 const { useState } = React
+interface TRowAST {
+    tag: string,
+    css: string[],
+    style: any[],
+    innerText?: string,
+    children: TRowAST[]
+}
 
 interface Tprops{
-    html: string
+    html: string,
+    tree: TRowAST[]
+}
+
+let randerTree = (tree: TRowAST[]) => {
+    return tree.map(e => <div className={e.css.join(' ')}>{
+            e.children.length > 0 ? randerTree(e.children) : 
+            (e.innerText || '')
+        }</div>
+    )
 }
 
 const _DebugLayout = (props: Tprops) => {
     return <div className="DebugLayout">
         <span>DebugLayout</span>
-        <div className="view-box" dangerouslySetInnerHTML={{__html: props.html}}></div>
+        <div className="view-box">{randerTree(props.tree)}</div>
     </div>
 }
 const stateMap = (state: any):Tprops => {
     return {
-        html: state.previewHTML
+        html: state.previewHTML,
+        tree: state.previewAST
     }
 }
 export const DebugLayout = connect(stateMap)(_DebugLayout)
