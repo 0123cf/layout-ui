87c43873357ec06491a4a7d28773c6580f6b8614
xxy <2416118450@qq.com>
2019-06-03 17:58:48 +0800

    test: add expect

--- tests/utils/utils.test.ts
+++ tests/utils/utils.test.ts
@@ -1,4 +1,4 @@
-import { getSTyleStr, id } from '../../src/utils/utils'
+import { getSTyleStr, id, setTreeData, addTreeData, delectTreeData } from '../../src/utils/utils'
 
 test('id', () => {
     expect(id(1)).toBe(1)
@@ -7,11 +7,82 @@ test('id', () => {
 })
 describe('css class utils getSTyleStr', () => {
     it('带大写的属性', () => {
-        expect(getSTyleStr({backgroundColor: "rgba(216,52,52,1)"}))
-        .toBe('background-color: rgba(216,52,52,1);')
+        expect(getSTyleStr({ backgroundColor: "rgba(216,52,52,1)" }))
+            .toBe('background-color: rgba(216,52,52,1);')
     })
     it('单个属性', () => {
-        expect(getSTyleStr({width: "30px", height: "30px"}))
-        .toBe('width: 30px;height: 30px;')
+        expect(getSTyleStr({ width: "30px", height: "30px" }))
+            .toBe('width: 30px;height: 30px;')
+    })
+})
+describe('test AST', () => {
+    it('set', () => {
+        expect(
+            addTreeData(
+                [{
+                    "tag": "div", "css": [""], "style": {},
+                    "children": []
+                }],
+                {
+                    path: [0, 0],
+                    childrenName: 'children',
+                    value: { "tag": "div", "css": ["flex", "al-flex-center-x", ""], "style": {}, "innerText": "aa", "children": [] }
+                }
+            )
+        ).toEqual([{
+            "tag": "div", "css": [""], "style": {},
+            "children": [
+                { "tag": "div", "css": ["flex", "al-flex-center-x", ""], "style": {}, "innerText": "aa", "children": [] }
+            ]
+        }])
+    })
+    it('add', () => {
+        expect(
+            setTreeData(
+                [{
+                    "tag": "div", "css": [""], "style": {},
+                    "children": []
+                }],
+                {
+                    path: [0, 0],
+                    childrenName: 'children',
+                    value: { "tag": "div", "css": ["flex", "al-flex-center-x"], "style": {}, "innerText": "QAQ", "children": [] }
+                }
+            )
+        ).toEqual([{
+            "tag": "div", "css": [""], "style": {},
+            "children": [
+                { "tag": "div", "css": ["flex", "al-flex-center-x"], "style": {}, "innerText": "QAQ", "children": [] }
+            ]
+        }])
+    })
+    it('del', () => {
+        expect(
+            delectTreeData(
+                [{
+                    "tag": "div", "css": [""], "style": {},
+                    "children": [{ "tag": "img" }]
+                }],
+                {
+                    path: [0, 0],
+                    childrenName: 'children'
+                }
+            )
+        ).toEqual([{
+            "tag": "div", "css": [""], "style": {},
+            "children": []
+        }])
+        expect(
+            delectTreeData(
+                [{
+                    "tag": "div", "css": [""], "style": {},
+                    "children": [{ "tag": "img" }]
+                }],
+                {
+                    path: [0],
+                    childrenName: 'children'
+                }
+            )
+        ).toEqual([])
     })
 })
